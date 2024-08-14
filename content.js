272e935cfec0fc083ee8da7f4b87707540345232
dario-piotrowicz <dario.piotrowicz@gmail.com>
2024-08-14 21:10:55 +0100

    improve solution so that the ai overview never appears

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
-const aiOverviewH1 = [...document.querySelectorAll('h1')].find(h1 => /ai overview/i.test(h1.innerText));
+const observer = new MutationObserver(() => {
+  // each time there's a mutation in the document see if there's an ai overview to hide
+  const aiOverviewH1 = [...document.querySelectorAll('h1')].find(h1 => /ai overview/i.test(h1.innerText));
 
-if(aiOverviewH1?.parentElement) {
-  aiOverviewH1.parentElement.style.display = 'none';
-}
+  if(aiOverviewH1?.parentElement) {
+    aiOverviewH1.parentElement.style.display = 'none';
+  }
+});
+
+observer.observe(document, {
+  childList: true,
+  subtree: true,
+});
