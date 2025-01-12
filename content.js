a02e25b4a10a02977230ef343fc3949873c17354
Zach Barnes <zbarnz@yahoo.com>
2025-01-12 14:53:53 -0700

    added martin between results and nav

--- content.js
+++ content.js
@@ -7,8 +7,13 @@ const observer = new MutationObserver(() => {
   // each time there's a mutation in the document see if there's an ai overview to hide
   const aiOverviewH1 = [...document.querySelectorAll('h1')].find(h1 => patterns.some(pattern => pattern.test(h1.innerText)));
 
-  if(aiOverviewH1?.parentElement) {
-    aiOverviewH1.parentElement.style.display = 'none';
+  if (aiOverviewH1?.parentElement) {
+    aiOverviewH1.parentElement.style.display = "none";
+  }
+
+  const mainElement = document.querySelector('[role="main"]');
+  if (mainElement) {
+    mainElement.style.marginTop = "24px";
   }
 });
 
